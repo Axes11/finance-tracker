53e36dc47cd8540c129ea96afdbec6e2ff2f81d3
Ivan <baklazhovivan2015@gmail.com>
2025-12-25 11:24:09 -0800

    feat: customize title bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
-// app/layout.tsx
 'use client';
 
 import './globals.css';
 import { fontSans } from '@/shared/lib/font';
 import { ReactNode } from 'react';
+import TitleBar from '@/shared/ui/title-bar.tsx';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang='en' className={`${fontSans.variable} font-sans`}>
-			<body>{children}</body>
+			<body>
+				<TitleBar />
+				<main>{children}</main>
+			</body>
 		</html>
 	);
 }
---
+++ src/shared/constants/colors.ts
@@ -0,0 +1,15 @@
+export const Colors = {
+	RED: '#ff5f57',
+	GREEN: '#ffbd2e',
+	YELLOW: '#28c840',
+	GREY: '#1a1a1a',
+};
+
+export const Color: Colors = {
+	RED: Colors.RED,
+	GREEN: Colors.GREEN,
+	YELLOW: Colors.YELLOW,
+	GREY: Colors.GREY,
+};
+
+export type Colors = typeof Colors;
